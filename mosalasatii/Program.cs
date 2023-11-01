58c2476b95e5776c75b14000850693056b552f33
uvhvy <uvhvy@DESKTOP-OM746DK>
2023-11-01 14:14:41 +0330

    Add project files.

---
+++ mosalasatii/Program.cs
@@ -0,0 +1,29 @@
+﻿//amir hossein hemmati
+//T.4 mosalasati
+long n;
+long m;
+long k;
+long x;
+string yesorno;
+do
+{
+    Console.WriteLine("adad n,om ra vared konid:");
+
+    n = Convert.ToInt64(Console.ReadLine());
+
+    Console.WriteLine("adad m,om ra vared konid:");
+
+    m = Convert.ToInt64(Console.ReadLine());
+
+    for (k = n; k <= m; k++)
+    {
+        x = k * (k + 1) / 2;
+
+        Console.WriteLine($"barabar ast ba : {x}");
+    }
+    Console.WriteLine("aya mikhahid edame dahid? bale/kheir");
+
+    yesorno = Console.ReadLine();
+
+}
+while (Console.ReadLine() == "bale");
